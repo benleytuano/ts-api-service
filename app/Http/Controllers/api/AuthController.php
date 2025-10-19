b1b2975f1d16b3dff0aafbc44e4f3a79c10873fd
benleytuano <benleyearltuano@gmail.com>
2025-10-20 04:50:02 +0800

    Changes to be committed:
    feat: Added controller for logout
    
    	modified:   app/Http/Controllers/api/AuthController.php

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -40,6 +40,17 @@ class AuthController extends Controller
 
     }
 
+    public function logout(Request $request, AuthService $service)
+    {
+        $result = $service->logout($request->user()); 
+
+        return response()->json([
+            'success' => $result,
+            'message' => 'Logged out successfully',
+        ]);
+
+    }
+
     public function me(AuthService $service)
     {
         $result = $service->getCurrentUser();
